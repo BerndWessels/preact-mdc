3378054a9564d909d344a9f5eb0fa5b6dab0d100
BerndWessels <wesselsbernd@gmail.com>
2017-04-11 14:56:17 +1200

    Preparing for build process.

--- material-ripple/index.js
+++ material-ripple/index.js
@@ -1,1 +1,3 @@
-export default process.env.WEB ? require('@material/ripple/dist/mdc.ripple.js').MDCRipple : function(){};
+const isBrowser = new Function("try {return this===window;}catch(e){ return false;}");
+
+export default isBrowser() ? require('@material/ripple/dist/mdc.ripple.js').MDCRipple : null;
